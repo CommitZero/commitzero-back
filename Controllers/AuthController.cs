3a8b6ab13ff8896a953c12ff318031976ff9a69e
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-10 18:29:37 -0300

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CommitZeroBack.Models;
+
+namespace CommitZeroBack.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Vazio");
+        }
+    }
+}
