86071ea0d2445c9234e1c7925b2110efba3ed9a8
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-09-18 15:03:01 -0300

    Update TestController.cs

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -9,7 +9,7 @@ namespace CommitZeroBack.Controllers
     {
         [HttpGet]
         public IActionResult Teste() {
-            return Content(ConfigManager.connection_string());
+            return Content(ConfigManager.api_key());
         }
         [HttpGet]
         public IActionResult Login()
